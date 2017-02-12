baf95d596a0a1c40320a473b8737a6ad8b39de74
ayushchopra96 <ayushchopra96@gmail.com>
2017-02-12 22:24:34 +0530

    classifier trained

--- Diabetes/views.py
+++ Diabetes/views.py
@@ -4,7 +4,35 @@ from register.models import MyUser
 from .models import Test
 from django.views.decorators.http import require_http_methods, require_GET, require_POST
 from django.views.decorators.csrf import csrf_exempt
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
 # Create your views here.
 @require_POST
 def request_test(request):
 	input = request.POST
+	response_data = {}
+	obj = Test.objects.create(username = input['username'])
+	response_data['success'] = '1'
+
+@require_POST
+def detect_diabetes(request):
+	input = request.POST
+
+
+
+model = SVC(kernel="linear", C=0.1)
+
+def predict_result(input):
+	global model
+	return model.predict(input)
+
+def model_train():
+	global model 
+	fd = open("diabetes.csv")
+	fd.readline()
+	data = np.loadtxt(f, delimiter=',')
+	X = data[:,:-1]
+	y = data[:,-1]
+	X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.20,random_state=0)
+	model = SVC(kernel="linear", C=0.15)
+	model.fit(X_train,y_train)
