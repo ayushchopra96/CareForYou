65eb2e0510c9e9249fe75026e37ddd9ce57bc27f
ayushchopra96 <ayushchopra96@gmail.com>
2017-02-12 20:52:02 +0530

    module 2 init

--- Diabetes/models.py
+++ Diabetes/models.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from register.models import MyUser
 # Create your models here.
+class Test(models.Model):
+	pass
--- register/admin.py
+++ register/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from register.models import MyUser
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+	list_display = ('uid','username','phone')
+	class Meta:
+		model = MyUser
+
+admin.site.register(MyUser,UserAdmin)
